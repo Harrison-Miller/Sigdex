2c2a2de080012ff1e9ac292405ce352b2f7b324e
Harrison Miller <hdmscratched@gmail.com>
2025-08-06 21:33:27 -0500

    setup dev variants

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,5 @@
+export const devVariantName = 'dev';
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-export const SIGDEX_VERSION = __APP_VERSION__;
+export const SIGDEX_VERSION = devVariantName ? `${__APP_VERSION__}-${devVariantName}` : __APP_VERSION__;
